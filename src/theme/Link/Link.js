3c8b895d307359ecdb8411137cc8d936c23a9925
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2018-09-19 08:46:07 +0100

    Add generic Link component

---
+++ src/theme/Link/Link.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Link.css';
+
+const Link = props => {
+  const { t, url, title, blank } = props;
+  return (
+    <a
+      href={url}
+      title={title}
+      target={blank ? '_blank' : '_self'}
+      className="link"
+    >
+      {t}
+    </a>
+  );
+};
+
+Link.propTypes = {
+  t: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  blank: PropTypes.bool,
+};
+
+Link.defaultProps = {
+  blank: false,
+};
+
+export default Link;
