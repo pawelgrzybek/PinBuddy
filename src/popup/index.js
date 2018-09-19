199bc6acc917905b48c59918b532d80912f092b1
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2018-09-19 16:27:04 +0100

    add redux-thunk to a project

--- src/popup/index.js
+++ src/popup/index.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import App from './components/App';
 import reducers from './reducers';
 import './index.css';
 
-const store = createStore(reducers);
+const store = createStore(reducers, applyMiddleware(thunk));
 
 // store.subscribe(() => {
-//   console.log('----------------------------------------------');
+// console.log('----------------------------------------------');
+// console.log(' ');
+// console.log('Redux state');
+// console.log(store.getState());
+// console.log(' ');
+// console.log('----------------------------------------------');
+// console.log('Chrome storage');
+// chrome.storage.local.get(result => {
+//   console.log(result);
 //   console.log(' ');
-//   console.log('Redux state');
-//   console.log(store.getState());
-//   console.log(' ');
 //   console.log('----------------------------------------------');
-//   console.log('Chrome storage');
-//   chrome.storage.local.get(result => {
-//     console.log(result);
-//     console.log(' ');
-//     console.log('----------------------------------------------');
-//   });
+// });
 // });
 
 render(
