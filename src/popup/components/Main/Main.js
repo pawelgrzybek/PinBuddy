3e240efda6601eee466ab88ecbda3b96571d93b1
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2018-09-21 11:53:46 +0100

    Main component code refactoring

--- src/popup/components/Main/Main.js
+++ src/popup/components/Main/Main.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+
 import Offline from '../Offline';
 import Online from '../Online';
 
@@ -12,11 +13,9 @@ Main.propTypes = {
   online: PropTypes.bool.isRequired,
 };
 
-const mapStateToProps = (state, ownProps) => {
-  return {
-    online: state.online,
-  };
-};
+const mapStateToProps = state => ({
+  online: state.online,
+});
 
 export default connect(mapStateToProps)(Main);
 
