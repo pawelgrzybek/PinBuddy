ff66c344b21ec3d61f33d1ed0140be252d161b9b
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2018-09-22 23:21:43 +0100

    tiny article css tweaks

--- src/popup/components/Article/Article.js
+++ src/popup/components/Article/Article.js
@@ -1,40 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import './Article.css';
 
-const Article = props => {
-  const {
-    privatePost,
-    unread,
-    href,
-    description,
-    time,
-    tags,
-  } = props;
+class Article extends Component {
+  state={
 
-  const timeFormated = time.substring(0, 10).replace(/-/g, '.');
-  const tagsFormated = tags.split(' ').map(tag => `#${tag}`).join(' ');
+  }
 
-  return (
-    <article className={`artile ${privatePost ? 'article--private' : ''}`}>
-      <a
-        className={`article__url ${unread ? 'article__url--unread' : ''}`}
-        href={href}
-        rel="noopener noreferrer"
-        target="_blank"
-      >
-        {description}
-      </a>
-      <div className="article__meta">
-        <span className="article__info">{timeFormated}</span>
-        {tags ? <span className="article__info article__info--tags">{tagsFormated}</span> : ''}
-        <span className="article__info article__info--action">Edit</span>
-        <span className="article__info article__info--action">Delete</span>
-      </div>
-    </article>
-  );
-};
+  render() {
+    const {
+      privatePost,
+      unread,
+      href,
+      description,
+      time,
+      tags,
+    } = this.props;
+
+    const timeFormated = time.substring(0, 10).replace(/-/g, '.');
+    const tagsFormated = tags.split(' ').map(tag => `#${tag}`).join(' ');
+
+    return (
+      <article className={`artile ${privatePost ? 'article--private' : ''}`}>
+        <a
+          className={`article__url ${unread ? 'article__url--unread' : ''}`}
+          href={href}
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          {description}
+        </a>
+        <div className="article__meta">
+          <span className="article__info">{timeFormated}</span>
+          {tags ? <span className="article__info article__info--tags">{tagsFormated}</span> : ''}
+          <span className="article__info article__info--edit">Edit</span>
+          <span className="article__info article__info--delete article__info--mouseover">Delete</span>
+        </div>
+      </article>
+    );
+  }
+}
+
 
 Article.propTypes = {
   privatePost: PropTypes.bool.isRequired,
