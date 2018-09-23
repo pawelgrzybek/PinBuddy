5d07bea6a3703ace972d0af8865d4dce200bb569
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2018-09-23 10:35:41 +0100

    add thunks to redux store

--- src/options/index.js
+++ src/options/index.js
@@ -1,26 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import App from './components/App';
 import reducers from './reducers';
 import 'theme/index.css';
 
-const store = createStore(reducers);
-
-// store.subscribe(() => {
-//   console.log('----------------------------------------------');
-//   console.log(' ');
-//   console.log('Redux state');
-//   console.log(store.getState());
-//   console.log(' ');
-//   console.log('Chrome storage');
-//   chrome.storage.local.get(result => {
-//     console.log(result);
-//     console.log(' ');
-//     console.log('----------------------------------------------');
-//   });
-// });
+const store = createStore(reducers, applyMiddleware(thunk));
 
 render(
   <Provider store={store}>
