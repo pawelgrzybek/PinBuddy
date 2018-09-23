8ad6419ca250f9ebbf85665d0c5790d7a6f6faec
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2018-09-23 23:34:27 +0100

    Add page

--- src/popup/components/Add/Add.js
+++ src/popup/components/Add/Add.js
@@ -1,11 +1,50 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import { postsAdd } from '../../actions/posts';
+import { Input, Button, Checkbox } from 'theme';
 import './Add.css';
-import { Input, Button } from 'theme';
 
 class Add extends Component {
   state ={
-
+    title: '',
+    url: '',
+    description: '',
+    tags: '',
+    privatePost: false,
+    readLater: false,
   };
+
+  refInput = React.createRef();
+
+  componentDidMount() {
+    chrome.tabs.query({ active: true }, tab => {
+      const { title, url } = tab[0];
+
+      this.setState({
+        title,
+        url,
+      });
+    });
+
+    chrome.tabs.executeScript({ code: 'window.getSelection().toString()' }, selection => {
+      if (selection) {
+        this.setState({
+          description: selection[0],
+        });
+      }
+    });
+
+    chrome.storage.sync.get(['privateCheckboxByDefault', 'toReadChecboxByDefault'], options => {
+      this.setState({
+        privatePost: options.privateCheckboxByDefault,
+        readLater: options.toReadChecboxByDefault,
+      });
+    });
+
+    this.refInput.current.focus();
+  }
 
   render() {
     return (
@@ -14,30 +53,82 @@ class Add extends Component {
           id="title"
           label="Title"
           placeholder="Title"
-          onChange={e => console.log(e)}
+          value={this.state.title}
+          onChange={this.updateFiled}
         />
 
         <Input
           id="description"
           label="Description"
           placeholder="Description"
-          onChange={e => console.log(e)}
+          value={this.state.description}
+          onChange={this.updateFiled}
         />
 
         <Input
           id="tags"
           label="Tags"
           placeholder="Tags"
-          onChange={e => console.log(e)}
+          value={this.state.tags}
+          onChange={this.updateFiled}
+          ref={this.refInput}
         />
+
+        <div className="add__options">
+          <div className="add__option">
+            <Checkbox
+              id="privatePost"
+              label="private"
+              checked={this.state.privatePost}
+              onChange={this.handleOptionToggle}
+            />
+          </div>
+          <div className="add__option">
+            <Checkbox
+              id="readLater"
+              label="read later"
+              checked={this.state.readLater}
+              onChange={this.handleOptionToggle}
+            />
+          </div>
+        </div>
 
         <Button
           t="Add URL"
+          disabled={!this.state.title}
+          onClick={this.handleAddUrlButton}
         />
       </div>
     );
   }
+
+  updateFiled = e => {
+    const { id, value } = e.target;
+    this.setState({
+      [id]: value,
+    });
+  }
+
+  handleOptionToggle = e => {
+    const { id, checked } = e.target;
+    this.setState({
+      [id]: checked,
+    });
+  }
+
+  handleAddUrlButton = () => {
+    this.props.postsAdd(this.state);
+  }
 }
 
-export default Add;
+Add.propTypes = {
+  postsAdd: PropTypes.func.isRequired,
+};
 
+const mapDispatchToProps = dispatch => {
+  return {
+    postsAdd: postInfo => dispatch(postsAdd(postInfo)),
+  };
+};
+
+export default connect(null, mapDispatchToProps)(Add);
