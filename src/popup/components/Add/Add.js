560b3c662d89ea0b77054d6a6e656546a539bd3d
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2018-09-23 13:35:18 +0100

    scafold an add window

--- src/popup/components/Add/Add.js
+++ src/popup/components/Add/Add.js
@@ -1,8 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './Add.css';
+import { Input, Button } from 'theme';
 
-const Add = props => (
-  <p>add</p>
-);
+class Add extends Component {
+  state ={
+
+  };
+
+  render() {
+    return (
+      <div className="add">
+        <Input
+          id="title"
+          label="Title"
+          placeholder="Title"
+          onChange={e => console.log(e)}
+        />
+
+        <Input
+          id="description"
+          label="Description"
+          placeholder="Description"
+          onChange={e => console.log(e)}
+        />
+
+        <Input
+          id="tags"
+          label="Tags"
+          placeholder="Tags"
+          onChange={e => console.log(e)}
+        />
+
+        <Button
+          t="Add URL"
+        />
+      </div>
+    );
+  }
+}
 
 export default Add;
 
