db436f1e58a151f3e954b5bcd83b88b1c0173468
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2018-09-25 18:15:01 +0100

    Simplify component LoggedOutView

--- src/popup/components/App/components/Main/components/Online/components/LoggedOutView/LoggedOutView.js
+++ src/popup/components/App/components/Main/components/Online/components/LoggedOutView/LoggedOutView.js
@@ -4,25 +4,26 @@ import { Paragraph, Button } from 'theme';
 class LoggedOutView extends PureComponent {
   render() {
     return (
-      <>
+      <div>
+
         <Paragraph
           innerHTML
           t={chrome.i18n.getMessage('popupLoggedOutViewMessage')}
         />
+
         <Button
           t={chrome.i18n.getMessage('popupLoggedOutViewButtonMessage')}
           onClick={this.handeOpenOptionPageButtonClick}
         />
-      </>
+
+      </div>
     );
   }
 
-  handeOpenOptionPageButtonClick = () => {
-    chrome.tabs.create({
-      url: '/options/options.html',
-    });
-  }
+  handeOpenOptionPageButtonClick = () => chrome.tabs.create({
+    url: '/options/options.html',
+  });
+
 }
 
 export default LoggedOutView;
-
