82cf80edac8ce4814a2c157b7a5365c258299582
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2018-09-26 11:10:15 +0100

    listen for icon change only when user is logged in

--- src/background/index.js
+++ src/background/index.js
@@ -1,50 +1,44 @@
-// update extensions icon
-const setIcon = postExists => {
-  chrome.browserAction.setIcon({
-    path: postExists ? '/icons/icon-active-16.png' : '/icons/icon-16.png',
+// icon change when url exists
+const listenForIconChange = tab => {
+  const { url } = tab;
+  chrome.storage.local.get(['posts'], posts => {
+    if (posts.posts) {
+      const postExists = !!posts.posts.find(post => post.href === url);
+      chrome.browserAction.setIcon({
+        path: postExists ? '/icons/icon-active-16.png' : '/icons/icon-16.png',
+      });
+    }
   });
 };
 
-// listen to url updates to check for icons change
+// listen to url updates
 chrome.tabs.onUpdated.addListener(id => {
-  chrome.tabs.get(id, tabInfo => {
-    const { url } = tabInfo;
-    chrome.storage.local.get(['posts'], posts => {
-      const postExists = !!posts.posts.find(post => post.href === url);
-      setIcon(postExists);
-    });
-  });
+  chrome.tabs.get(id, listenForIconChange);
 });
 
-// listen to new tabs to check for icons change
+// listen to new tabs
 chrome.tabs.onActivated.addListener(tab => {
-  chrome.tabs.get(tab.tabId, tabInfo => {
-    const { url } = tabInfo;
-    chrome.storage.local.get(['posts'], posts => {
-      const postExists = !!posts.posts.find(post => post.href === url);
-      setIcon(postExists);
-    });
-  });
+  chrome.tabs.get(tab.tabId, listenForIconChange);
 });
 
-chrome.runtime.onInstalled.addListener(e => {
-  chrome.contextMenus.create({
-    id: 'addToPinboard',
-    title: 'Add to Pinboard',
-  });
+// chrome.runtime.onInstalled.addListener(e => {
+//   chrome.contextMenus.create({
+//     id: 'addToPinboard',
+//     title: 'Add to Pinboard',
+//   });
 
-  chrome.contextMenus.create({
-    id: 'addToPinboardWithDescription',
-    title: 'Add to Pinboard (with description)',
-    contexts: ['selection']
-  });
+//   chrome.contextMenus.create({
+//     id: 'addToPinboardWithDescription',
+//     title: 'Add to Pinboard (with description)',
+//     contexts: ['selection']
+//   });
 
-  chrome.contextMenus.create({
-    id: 'addLinkToPinboard',
-    title: 'Add link to Pinboard',
-    contexts: ['link']
-  });
-});
+//   chrome.contextMenus.create({
+//     id: 'addLinkToPinboard',
+//     title: 'Add link to Pinboard',
+//     contexts: ['link']
+//   });
+// });
 
 // chrome.commands.onCommand.addListener(command => {
 //   console.log('Command:', command);
