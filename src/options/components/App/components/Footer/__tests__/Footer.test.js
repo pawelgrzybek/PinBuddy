bae66421098d8cf71896bd97a996ab229d8b8590
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2018-10-11 14:40:32 +0100

    Footer tests

---
+++ src/options/components/App/components/Footer/__tests__/Footer.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Footer from '../Footer';
+
+describe('<Footer />', () => {
+
+  it('should render correctly', () => {
+    const tree = shallow(
+      <Footer />
+    );
+    expect(tree).toMatchSnapshot();
+  });
+
+});
